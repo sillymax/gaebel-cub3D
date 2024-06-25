59ec57d82e5dbdc9d670eb69391495e0f0539ea7
ychng <ychng@student.42kl.edu.my>
2024-06-25 16:24:15 +0800

    doing can_parse_mapcontent(t_main *main, int fd);

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 03:43:34 by ychng             #+#    #+#             */
-/*   Updated: 2024/06/25 04:02:46 by ychng            ###   ########.fr       */
+/*   Updated: 2024/06/25 15:00:07by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,14 @@ typedef struct s_data
 	char			*east_texture;
 	char			*sprite_texture;
 	unsigned short	floorcolor[3];
+	bool			is_width_set;
+	bool			is_height_set;
+	bool			is_north_texture_set;
+	bool			is_south_texture_set;
+	bool			is_west_texture_set;
+	bool			is_east_texture_set;
+	bool			is_sprite_texture_set;
+	bool			is_floorcolor_set;
 }	t_data;
 
 typedef struct s_map
@@ -40,7 +48,7 @@ typedef struct s_main
 
 bool	exetension_is_cub(char *mapname)
 {
-	short	count;
+	size_t	count;
 	char	*token;
 
 	count = 0;
@@ -103,7 +111,7 @@ bool	is_all_digits(char *str)
 	return (all_digits);
 }
 
-bool	is_within_unsigned_short(char *value_str)
+bool	is_in_unsigned_short_range(char *value_str)
 {
 	int	value;
 
@@ -111,7 +119,7 @@ bool	is_within_unsigned_short(char *value_str)
 	return (value >= 0 && value <= USHRT_MAX);
 }
 
-void	set_resolution(t_main *main, char *line)
+int	set_resolution(t_main *main, char *line)
 {
 	char	*width_str;
 	char	*height_str;
@@ -119,22 +127,16 @@ void	set_resolution(t_main *main, char *line)
 	width_str = ft_strtok(NULL, " ");
 	height_str = ft_strtok(NULL, " ");
 	if (!(width_str && height_str))
-	{
-		free(line);
-		exit_with_message("error: resolution must have both width and height.");
-	}
+		return (error_with_message("error: resolution must have both width and height."));
 	if (!(is_all_digits(width_str) && is_all_digits(height_str)))
-	{
-		free(line);
-		exit_with_message("error: resolution dimensions must be valid number.");
-	}
-	if (!(is_within_unsigned_short(width_str) && is_within_unsigned_short(height_str)))
-	{
-		free(line);
-		exit_with_messsage("error: resolution dimensions are out of range.");
-	}
+		return (error_with_message("error: resolution dimensions must be valid number."));
+	if (!(is_within_unsigned_short(width_str) && is_in_unsigned_short_range(height_str)))
+		return (error_with_message("error: resolution dimensions are out of range."));
 	main->data.width = (unsigned short)ft_atoi(width_str);
 	main->data.height = (unsigned short)ft_atoi(height_str);
+	main->data.is_width_set = true;
+	main->data.is_height_set = true;
+	return (SUCCESS);
 }
 
 bool	is_valid_path(char *path)
@@ -142,28 +144,32 @@ bool	is_valid_path(char *path)
 	return (!ft_strncmp(path, "./", 2) && ft_isalnum(path[2]));
 }
 
-void	set_texture(char **texture, char *line)
+int	set_texture(char **texture, bool *is_set, char *line)
 {
 	char	*path;
 
 	path = ft_strtok(NULL, " ");
 	if (!path)
-	{
-		free(line);
-		exit_with_messahe("error: missing path.");
-	}
+		return (error_with_message("error: missing path."));
 	if (!is_valid_path(path))
-	{
-		free(line);
-		exit_with_message("error: invalid path.");
-	}
+		return (error_with_message("error: invalid path."));
 	*texture = path;
+	*is_set = true;
+	return (SUCCESS);
 }
 
-void	validate_floorcolor_format(char *floorcolor, char *line)
+bool	is_in_rgb_range(char *value_str)
 {
-	size_t	count;
-	char	*token;
+	int	value;
+
+	value = ft_atoi(value_str);
+	return (value >= 0 && value <= 255);
+}
+
+int	validate_floorcolor_format(char *floorcolor, char *line)
+{
+	unsigned short	count;
+	char			*token;
 
 	count = 0;
 	while (count < 3)
@@ -172,40 +178,35 @@ void	validate_floorcolor_format(char *floorcolor, char *line)
 			token = ft_strtok(floorcolor, ",");
 		else
 			token = ft_strtok(NULL, ",");
-		if (!(token && is_all_digits(token) && is_valid_path(token)))
-		{
-			free(line);
-			exit_with_message("error: invalid floor color format.");
-		}
+		if (!(token && is_all_digits(token) && is_in_rgb_range(token)))
+			return (ERROR);
 	}
 	token = ft_strtok(NULL, ",");
 	if (token)
-	{
-		free(line);
-		exit_with_message("error: invalid floor color format.");
-	}
+		return (ERROR);
+	return (SUCCESS);
 }
 
-void	set_floorcolor(t_main *main, char *line)
+int	set_floorcolor(t_main *main, char *line)
 {
 	char	*floorcolor;
 	char	*floorcolor_copy;
 
 	floorcolor = ft_strtok(NULL, " ");
 	if (!floorcolor)
-	{
-		free(line);
-		exit_with_message("error: missing floor color.");
-	}
+		return (error_with_message("error: missing floor color."));
 	floorcolor_copy = ft_strdup(floorcolor);
-	validate_floorcolor_format(floorcolor_copy, line);
+	if (validate_floorcolor_format(floorcolor_copy, line) == ERROR)
+		return (error_with_message("error: invalid floor color format."));
 	free(floorcolor_copy);
 	main->data.floorcolor[0] = (unsigned short)ft_atoi(ft_strtok(floorcolor, ","));
 	main->data.floorcolor[1] = (unsigned short)ft_atoi(ft_strtok(NULL, ","));
 	main->data.floorcolor[2] = (unsigned short)ft_atoi(ft_strtok(NULL, ","));
+	main->data.is_floorcolor_set = true;
+	return (SUCCESS);
 }
 
-void	set_identifiers_value(t_main *main, char *line)
+int	set_identifiers_value(t_main *main, char *line)
 {
 	char	*token;
 
@@ -213,49 +214,117 @@ void	set_identifiers_value(t_main *main, char *line)
 	if (token)
 	{
 		if (ft_strcmp(token, "R"))
-			set_resolution(main, line);
+			return (set_resolution(main, line));
 		else if (ft_strcmp(token, "NO"))
-			set_texture(&main->data.north_texture, line);
+			return (set_texture(&main->data.north_texture, &main->data.is_north_texture_set, line));
 		else if (ft_strcmp(token, "SO"))
-			set_texture(&main->data.south_texture, line);
+			return (set_texture(&main->data.south_texture, &main->data.is_south_texture_set, line));
 		else if (ft_strcmp(token, "WE"))
-			set_texture(&main->data.west_testure, line);
+			return (set_texture(&main->data.west_testure, &main->data.is_west_texture_set, line));
 		else if (ft_strcmp(token, "EA"))
-			set_texture(&main->data.east_texture, line);
+			return (set_texture(&main->data.east_texture, &main->data.is_east_texture_set, line));
 		else if (ft_strcmp(token, "S"))
-			set_texture(&main->data.sprite_texture, line);
+			return (set_texture(&main->data.sprite_texture, &main->data.is_south_texture_set, line));
 		else if (ft_strcmp(token, "F"))
-			set_floorcolor(main, line);
+			return (set_floorcolor(main, line));
 	}
 	free(token);
+	return (SUCCESS);
 }
 
-bool	parse_mapdata(t_main *main, char *mapname)
+bool	all_identifiers_set(t_main *main)
 {
-	int		fd;
+	return (main->data.is_width_set && \
+			main->data.is_height_set && \
+			main->data.is_north_texture_set && \
+			main->data.is_south_texture_set && \
+			main->data.is_west_texture_set && \
+			main->data.is_east_texture_set && \
+			main->data.is_sprite_texture_set && \
+			main->data.is_floorcolor_set);
+}
+
+// Free the line before the last return, in case the loop breaks due to all_identifiers_set(main)
+bool	can_parse_mapdata(t_main *main, int fd)
+{
 	char	*line;
 
-	fd = open(mapname, O_RDONLY);
-	if (fd < 0)
-		exit_with_message("error: opening file.");
+	while (true)
+	{
+		line = get_next_line(fd);
+		if (!line || all_identifiers_set(main))
+			break ;
+		else if (set_identifiers_value(main, line) == ERROR)
+		{
+			free(line);
+			return (false);
+		}
+		free(line);
+	}
+	free(line);
+	return (all_identifiers_set(main));
+}
+
+bool	can_parse_mapcontent(t_main *main, int fd)
+{
+	char	*line;
+
 	while (true)
 	{
 		line = get_next_line(fd);
 		if (!line)
 			break ;
-		set_identifiers_value(main, line);
-		free(line);
 	}
+	return ();
 }
 
 void	parse_map(t_main *main, char *mapname)
 {
+	int	fd;
+
 	if (!extension_is_cub(mapname))
 		exit_with_message("error: invalid map extension.");
-	if (!parse_mapdata(main, mapname))
-		exit_with_message("");
-	if (!parse_mapcontent())
-		exit_with_message("");
+	fd = open(mapname, O_RDONLY);
+	if (fd < 0)
+		exit_with_message("error: opening map.");
+	if (!can_parse_mapdata(main, fd))
+	{
+		close(fd);
+		exit_with_message("error: parsing map data.");
+	}
+	if (!can_parse_mapcontent(main, fd))
+	{
+		close(fd);
+		exit_with_message("error: parsing map content.");
+	}
+	close(fd);
+}
+
+void	initialize_data(t_data *data)
+{
+	data->width = 0;
+	data->height = 0;
+	data->north_texture = NULL;
+	data->south_texture = NULL;
+	data->west_testure = NULL;
+	data->east_texture = NULL;
+	data->sprite_texture = NULL;
+	data->floorcolor[0] = 0;
+	data->floorcolor[1] = 0;
+	data->floorcolor[2] = 0;
+	data->is_width_set = false;
+	data->is_height_set = false;
+	data->is_north_texture_set = false;
+	data->is_south_texture_set = false;
+	data->is_west_texture_set = false;
+	data->is_east_texture_set = false;
+	data->is_sprite_texture_set = false;
+	data->is_floorcolor_set = false;
+}
+
+void	initialize_main(t_main *main)
+{
+	intialize_data(&main->data);
 }
 
 int main(int argc, char **argv)
@@ -264,5 +333,6 @@ int main(int argc, char **argv)
 
 	// if (!(argc == 2))
 		// return (write_error());
+	initialize_main(&main);
 	parse_map(&main, argv[1]);
 }
