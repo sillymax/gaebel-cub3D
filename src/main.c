9535962947c24678e4ef6cb26fc00a88ac35bf1e
ychng <ychng@student.42kl.edu.my>
2024-06-26 02:20:59 +0800

    doing validate_2dmap

--- src/main.c
+++ src/main.c
@@ -35,8 +35,8 @@ typedef struct s_data
 
 typedef struct s_map
 {
-	size_t	rows;
-	size_t	columns;
+	size_t	used_rows;
+	size_t	alloc_rows;	
 	char	**map2d;
 }	t_map;
 
@@ -119,7 +119,7 @@ bool	is_in_unsigned_short_range(char *value_str)
 	return (value >= 0 && value <= USHRT_MAX);
 }
 
-int	set_resolution(t_main *main, char *line)
+int	set_resolution(t_data *data, char *line)
 {
 	char	*width_str;
 	char	*height_str;
@@ -132,10 +132,10 @@ int	set_resolution(t_main *main, char *line)
 		return (error_with_message("error: resolution dimensions must be valid number."));
 	if (!(is_within_unsigned_short(width_str) && is_in_unsigned_short_range(height_str)))
 		return (error_with_message("error: resolution dimensions are out of range."));
-	main->data.width = (unsigned short)ft_atoi(width_str);
-	main->data.height = (unsigned short)ft_atoi(height_str);
-	main->data.is_width_set = true;
-	main->data.is_height_set = true;
+	data->width = (unsigned short)ft_atoi(width_str);
+	data->height = (unsigned short)ft_atoi(height_str);
+	data->is_width_set = true;
+	data->is_height_set = true;
 	return (SUCCESS);
 }
 
@@ -187,7 +187,7 @@ int	validate_floorcolor_format(char *floorcolor, char *line)
 	return (SUCCESS);
 }
 
-int	set_floorcolor(t_main *main, char *line)
+int	set_floorcolor(t_data *data, char *line)
 {
 	char	*floorcolor;
 	char	*floorcolor_copy;
@@ -199,14 +199,14 @@ int	set_floorcolor(t_main *main, char *line)
 	if (validate_floorcolor_format(floorcolor_copy, line) == ERROR)
 		return (error_with_message("error: invalid floor color format."));
 	free(floorcolor_copy);
-	main->data.floorcolor[0] = (unsigned short)ft_atoi(ft_strtok(floorcolor, ","));
-	main->data.floorcolor[1] = (unsigned short)ft_atoi(ft_strtok(NULL, ","));
-	main->data.floorcolor[2] = (unsigned short)ft_atoi(ft_strtok(NULL, ","));
-	main->data.is_floorcolor_set = true;
+	data->floorcolor[0] = (unsigned short)ft_atoi(ft_strtok(floorcolor, ","));
+	data->floorcolor[1] = (unsigned short)ft_atoi(ft_strtok(NULL, ","));
+	data->floorcolor[2] = (unsigned short)ft_atoi(ft_strtok(NULL, ","));
+	data->is_floorcolor_set = true;
 	return (SUCCESS);
 }
 
-int	set_identifiers_value(t_main *main, char *line)
+int	set_identifiers_value(t_data *data, char *line)
 {
 	char	*token;
 
@@ -214,34 +214,34 @@ int	set_identifiers_value(t_main *main, char *line)
 	if (token)
 	{
 		if (ft_strcmp(token, "R"))
-			return (set_resolution(main, line));
+			return (set_resolution(data, line));
 		else if (ft_strcmp(token, "NO"))
-			return (set_texture(&main->data.north_texture, &main->data.is_north_texture_set, line));
+			return (set_texture(&data->north_texture, &data->is_north_texture_set, line));
 		else if (ft_strcmp(token, "SO"))
-			return (set_texture(&main->data.south_texture, &main->data.is_south_texture_set, line));
+			return (set_texture(&data->south_texture, &data->is_south_texture_set, line));
 		else if (ft_strcmp(token, "WE"))
-			return (set_texture(&main->data.west_testure, &main->data.is_west_texture_set, line));
+			return (set_texture(&data->west_testure, &data->is_west_texture_set, line));
 		else if (ft_strcmp(token, "EA"))
-			return (set_texture(&main->data.east_texture, &main->data.is_east_texture_set, line));
+			return (set_texture(&data->east_texture, &data->is_east_texture_set, line));
 		else if (ft_strcmp(token, "S"))
-			return (set_texture(&main->data.sprite_texture, &main->data.is_south_texture_set, line));
+			return (set_texture(&data->sprite_texture, &data->is_south_texture_set, line));
 		else if (ft_strcmp(token, "F"))
-			return (set_floorcolor(main, line));
+			return (set_floorcolor(data, line));
 	}
 	free(token);
 	return (SUCCESS);
 }
 
-bool	all_identifiers_set(t_main *main)
+bool	all_identifiers_set(t_data *data)
 {
-	return (main->data.is_width_set && \
-			main->data.is_height_set && \
-			main->data.is_north_texture_set && \
-			main->data.is_south_texture_set && \
-			main->data.is_west_texture_set && \
-			main->data.is_east_texture_set && \
-			main->data.is_sprite_texture_set && \
-			main->data.is_floorcolor_set);
+	return (data->is_width_set && \
+			data->is_height_set && \
+			data->is_north_texture_set && \
+			data->is_south_texture_set && \
+			data->is_west_texture_set && \
+			data->is_east_texture_set && \
+			data->is_sprite_texture_set && \
+			data->is_floorcolor_set);
 }
 
 // Free the line before the last return, in case the loop breaks due to all_identifiers_set(main)
@@ -252,9 +252,9 @@ bool	can_parse_mapdata(t_main *main, int fd)
 	while (true)
 	{
 		line = get_next_line(fd);
-		if (!line || all_identifiers_set(main))
+		if (!line || all_identifiers_set(&main->data))
 			break ;
-		else if (set_identifiers_value(main, line) == ERROR)
+		else if (set_identifiers_value(&main->data, line) == ERROR)
 		{
 			free(line);
 			return (false);
@@ -262,20 +262,98 @@ bool	can_parse_mapdata(t_main *main, int fd)
 		free(line);
 	}
 	free(line);
-	return (all_identifiers_set(main));
+	return (all_identifiers_set(&main->data));
+}
+
+bool	is_empty_line(char *line)
+{
+	while (*line)
+	{
+		if (!ft_isblank(*line))
+			return (false);
+		line++;
+	}
+	return (true);
+}
+
+bool	has_valid_characters(char *line)
+{
+	char	*trimmed_line;
+	size_t	trim_len;
+	size_t	span_len;
+
+	trimmed_line = ft_strtrim(line, " ");
+	trim_len = ft_strlen(trimmed_line);
+	span_len = ft_strspn(line, "01NSEW");
+	free(trimmed_line);
+	return (trim_len == span_len);
+}
+
+int	store_map_in_2d_array(t_map *map, char *line)
+{
+	size_t	old_size;
+	size_t	new_size;
+	size_t	new_alloc_rows;
+	char	**new_map2d;
+
+	if (map->used_rows >= map->alloc_rows)
+	{
+		if (map->alloc_rows == 0)
+			new_alloc_rows = 1;
+		else
+			new_alloc_rows = map->alloc_rows * 2;
+		old_size = map->alloc_rows * sizeof(char *);
+		new_size = new_alloc_rows * sizeof(char *);
+		new_map2d = ft_realloc(map->map2d, old_size, new_size);
+		if (!map->map2d)	
+			return (error_with_message("error: failed to realloc memory."));
+		map->map2d = new_map2d;
+		map->alloc_rows = new_size;
+	}
+	map->map2d[map->used_rows] = ft_strdup(line);
+	map->used_rows++;
+	return (SUCCESS);
+}
+
+int	validate_2dmap(t_map *map)
+{
+	size_t	i;
+
+	i = 0;
+	while (map->map2d[i])	
+	{
+
+		i++;
+	}
 }
 
 bool	can_parse_mapcontent(t_main *main, int fd)
 {
 	char	*line;
+	bool	map_started;
 
+	map_started = false;
 	while (true)
 	{
 		line = get_next_line(fd);
 		if (!line)
 			break ;
+		if ((map_started == false) && (is_empty_line(line)))
+			continue ;
+		map_started = true;
+		if ((map_started == true) && (is_empty_line(line)))
+		{
+			free(line);
+			return (false);
+		}
+		if (store_map_in_2d_array(&main->map, line) == ERROR);
+		{
+			free(line);
+			return (false);
+		}
+		free(line);
 	}
-	return ();
+	return (validate_2dmap(&main->map) == SUCCESS);
 }
 
 void	parse_map(t_main *main, char *mapname)
@@ -322,9 +400,18 @@ void	initialize_data(t_data *data)
 	data->is_floorcolor_set = false;
 }
 
+void	initialize_data(t_map *map)
+{
+	map->used_rows = 0;
+	map->alloc_rows = 0;
+	map->map2d = NULL;
+}
+
+
 void	initialize_main(t_main *main)
 {
-	intialize_data(&main->data);
+	initialize_data(&main->data);
+	initialize_map(&main->map);
 }
 
 int main(int argc, char **argv)
