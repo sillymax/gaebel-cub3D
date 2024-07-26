2ad83b57564873c90f796128f37b9e396374b1e5
ychng <ychng@student.42kl.edu.my>
2024-07-26 23:40:14 +0800

    fixed fish bowl effect 1 variable

--- src/3d_projection/cast_wall_strips.c
+++ src/3d_projection/cast_wall_strips.c
@@ -6,23 +6,19 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 18:31:08 by ychng             #+#    #+#             */
-/*   Updated: 2024/07/26 19:06:47 by ychng            ###   ########.fr       */
+/*   Updated: 2024/07/26 23:39:32 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-void	draw_rect(t_main *main, int wall_strip_height, int i)
+void	draw_rect(t_main *main, int width, int height, int i)
 {
-	int	height;
-	int	width;
 	int	start_y;
 	int	start_x;
 	int	y;
-	int	x;	
+	int	x;
 
-	height = wall_strip_height;
-	width = main->raycast.wall_strip_width;
 	start_y = (main->mapdata.m_height / 2) - (height / 2);
 	start_x = i * width;
 	y = start_y;
@@ -42,12 +38,19 @@ void	draw_rect(t_main *main, int wall_strip_height, int i)
 
 void	draw_wall_strip(t_main *main, int i)
 {
+	t_ray	*ray;
+	double	correct_wall_distance;
 	double	distance_projection_plane;
 	int		wall_strip_height;
 
-	distance_projection_plane = (main->mapdata.m_width / 2) / tan(main->raycast.fov_angle / 2);
-	wall_strip_height = (TILE_SIZE / main->raycast.rays[i].distance) * distance_projection_plane;
-	draw_rect(main, wall_strip_height, i);
+	ray = &main->raycast.rays[i];
+	correct_wall_distance = \
+		ray->distance * cos(ray->ray_angle - main->player.rotation_angle);
+	distance_projection_plane = \
+		(main->mapdata.m_width / 2) / tan(main->raycast.fov_angle / 2);
+	wall_strip_height = \
+		(TILE_SIZE / correct_wall_distance) * distance_projection_plane;
+	draw_rect(main, main->raycast.wall_strip_width, wall_strip_height, i);
 }
 
 void	cast_wall_strips(t_main *main)
